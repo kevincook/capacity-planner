c6984efb5643d409f110f397ac1def496e1c9888
Kevin Cook <kevin.cook@gmail.com>
2009-04-22 14:53:05 -0500

    plugins

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+ActionController::Base.session = { :session_key => '_LUBLYV_session',
+:secret => 'YNQNLTDDJVCRDMVUJPOEJJXQJIDXLZAITFUEAAZE' }
+ActionController::Base.session_store = :active_record_store
